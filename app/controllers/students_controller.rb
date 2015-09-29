84d25bf1640ac18b5675f6c741850b40d3eea916
Dennis Liaw <dennis.liaw@gmail.com>
2015-09-28 21:30:07 -0400

    adds Chris' controllers

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,24 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find params[:id]
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create student_params
+    redirect_to student_path(student)
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :img_url, :house_id)
+  end
+
+end
